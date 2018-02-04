4a7064af994658703b2327bc1ae4ac9a035b6d8f
Saad Iqbal <s1iqbal@ryerson.ca>
2018-02-03 21:19:54 -0500

    updated node modules

--- server.js
+++ server.js
@@ -1,22 +1,49 @@
 const express = require('express');
 const hbs = require('hbs');
-
+const fs = require('fs');
 const app = express();
 
+hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
+app.set('trust proxy', true);
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  var ip = (req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress).split(",")[0];
+
+  console.log(log + '\n' + ip + '\n\n');
+  console.log(ip);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append server.log');
+    }
+  });
+  next();
+});
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
 
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home',
-    currentYear: new Date().getFullYear()
+    welcomeMessage: 'Welcome to this website.',
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 })
 
