aad5cc0388d36386ca3c666432279e39709efdbc
Saad Iqbal <s1iqbal@ryerson.ca>
2018-02-04 16:05:25 -0500

    server update

--- server.js
+++ server.js
@@ -59,17 +59,6 @@ app.get('/projects', (req, res) => {
   });
 });
 
-app.get('/about', (req, res) => {
-  res.render('about.hbs', {
-    pageTitle: 'About Page',
-  });
-})
-
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
